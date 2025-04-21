fd99a8ba2287a44b3cd0e4951e4af68787a323be
kemal.adiatna <drawzxv3@gmail.com>
2025-04-21 14:24:56 +0700

    v0.2.1 | 21 Apr 2025 | fix inconsistent windows file name;

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,6 @@ echo "Build codepush-cli-darwin-x64..."
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-darwin-x64"
 
 echo "Build codepush-cli-win-x64..."
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-win-x64"
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-win-x64.exe"
 
 echo "Build Complete"
