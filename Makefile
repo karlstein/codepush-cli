78ea8099aca33af6837c65ac83bf3fc4ceec5a77
kemal.adiatna <drawzxv3@gmail.com>
2025-04-21 13:58:20 +0700

    v0.2.0 | 21 Apr 2025 | setup npm package;

--- Makefile
+++ Makefile
@@ -2,11 +2,13 @@ include .env
 export $(shell sed 's/=.*//' .env)
 
 build:
+	@cd cli
+	@go mod download
 ifeq ($(GITHUB_ACTION),TRUE)
-	@go mod download
-	@CGO_ENABLED=0 go build -ldflags '-extldflags "-static"' -o "./bin/codepush-cli"
+	@CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-linux-x64"
+	@CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-darwin-x64"
+	@CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-windows-x64"
 else
-	@go mod download
 	@CGO_ENABLED=0 go build -ldflags '-extldflags "-static"' -o "${GOPATH}/bin/codepush-cli"
 endif
 	
---
+++ scripts/install.js
@@ -0,0 +1,59 @@
+import { get } from "https";
+import { createWriteStream } from "fs";
+import { join } from "path";
+import { platform as _platform, arch as _arch } from "os";
+import { chmodSync } from "fs";
+import * as dotenv from "dotenv";
+
+// Get __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env file
+dotenv.config({ path: join(__dirname, "..", ".env") });
+
+const platform = _platform();
+const arch = _arch();
+
+const ghPath = "https://github.com/karlstein/codepush-cli/releases/download";
+const currVersion = process.env.VERSION;
+let binaryName = "codepush-cli";
+let releaseName = "";
+
+if (platform === "darwin" && arch === "x64") {
+  releaseName = "codepush-cli-darwin-x64";
+} else if (platform === "linux" && arch === "x64") {
+  releaseName = "codepush-cli-linux-x64";
+} else if (platform === "win32" && arch === "x64") {
+  binaryName = "codepush-cli.exe";
+  releaseName = "codepush-cli-win-x64.exe";
+} else {
+  console.error(`Unsupported platform: ${platform} ${arch}`);
+  process.exit(1);
+}
+
+const downloadUrl = `${ghPath}/${currVersion}/${releaseName}`;
+const outputDir = join(__dirname, "..", "lib");
+const outputPath = join(outputDir, binaryName);
+
+// Download binary
+get(downloadUrl, (res) => {
+  if (res.statusCode !== 200) {
+    console.error(`Failed to download binary: ${res.statusCode}`);
+    process.exit(1);
+  }
+
+  const file = createWriteStream(outputPath);
+  res.pipe(file);
+
+  file.on("finish", () => {
+    file.close();
+    if (platform !== "win32") {
+      chmodSync(outputPath, 0o755);
+    }
+    console.log(`Downloaded CLI to ${outputPath}`);
+  });
+}).on("error", (err) => {
+  console.error(`Error downloading binary: ${err.message}`);
+  process.exit(1);
+});
