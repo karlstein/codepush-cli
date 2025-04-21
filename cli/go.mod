99183d6319a8ebe8ac4497e7fa65b1a2c016d155
kemal.adiatna <drawzxv3@gmail.com>
2025-04-21 14:20:14 +0700

    v0.2.0 | 21 Apr 2025 | setup npm package;

--- cli/go.mod
+++ cli/go.mod
@@ -4,12 +4,12 @@ go 1.22.1
 
 require (
 	github.com/aws/aws-sdk-go-v2 v1.36.3
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.62
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.10 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.17.62 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+cd cli
+echo "Download go modules..."
+go mod download
+
+echo "Build codepush-cli-linux-x64..."
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-linux-x64"
+
+echo "Build codepush-cli-darwin-x64..."
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-darwin-x64"
+
+echo "Build codepush-cli-win-x64..."
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "../bin/codepush-cli-win-x64"
+
+echo "Build Complete"
