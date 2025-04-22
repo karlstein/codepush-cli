2b4c67b5023cd0243c94527f4516e791515af0fd
kemal.adiatna <drawzxv3@gmail.com>
2025-04-22 14:15:59 +0700

    v0.2.7 | 22 Apr 2025 | move install.js to root;

---
+++ install.js
@@ -0,0 +1,114 @@
+import { get } from "https";
+import { createWriteStream, mkdir } from "fs";
+import { join, dirname } from "path";
+import { platform as _platform, arch as _arch } from "os";
+import { fileURLToPath } from "url";
+
+// Get __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const platform = _platform();
+const arch = _arch();
+
+const ghPath = "https://github.com/karlstein/codepush-cli/releases/download";
+const currVersion = "v0.2.6";
+let binaryName = "codepush-cli";
+let releaseName = "";
+
+if (platform === "darwin" && arch === "x64") {
+  releaseName = "codepush-cli-darwin-x64";
+} else if (platform === "linux" && arch === "x64") {
+  releaseName = "codepush-cli-linux-x64";
+} else if (platform === "win32" && arch === "x64") {
+  binaryName = "codepush-cli.exe";
+  releaseName = "codepush-cli-win-x64.exe";
+} else {
+  console.error(`Unsupported platform: ${platform} ${arch}`);
+  process.exit(1);
+}
+
+const downloadUrl = `${ghPath}/${currVersion}/${releaseName}`;
+const outputDir = join(__dirname, "..", "bin");
+
+mkdir(outputDir, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Directory created successfully!");
+});
+
+const outputPath = join(outputDir, binaryName);
+const isWin = process.platform === "win32";
+
+const logger = (msg) => {
+  if (isWin) {
+    process.stdout.write(`${msg}\n`);
+  } else {
+    console.error(msg);
+  }
+};
+
+function downloadAsset(assetUrl) {
+  console.error(`downloadAsset ${assetUrl}...`);
+
+  return new Promise((resolve, reject) => {
+    const url = new URL(assetUrl);
+    const headers = {
+      "User-Agent": "Node.js",
+      Accept: "application/octet-stream", // Required for binary downloads
+    };
+
+    const followRedirect = (response) => {
+      logger(
+        `followRedirect response ${response.statusCode} - ${response.headers.location}`
+      );
+
+      if ([301, 302, 307].includes(response.statusCode)) {
+        // GitHub returns a 302 redirect for asset downloads
+        const redirectUrl = response.headers.location;
+        get(redirectUrl, followRedirect).on("error", reject);
+      } else {
+        // console.table(response);
+        // Stream response to file
+        const file = createWriteStream(outputPath);
+        response.pipe(file);
+        file.on("finish", () => {
+          file.close();
+          resolve();
+        });
+      }
+    };
+
+    // Initial request to GitHub's asset endpoint
+    get(
+      {
+        hostname: url.hostname,
+        path: url.pathname,
+        headers,
+        timeout: 30000,
+      },
+      followRedirect
+    ).on("error", reject);
+  });
+}
+
+(async () => {
+  try {
+    // Download the asset
+    logger(`Downloading ${releaseName}...`);
+    const res = await downloadAsset(downloadUrl); // Use the asset's API URL (supports auth)
+
+    // downloadAsset(downloadUrl)
+    //   .then((e) => {
+    //     logger(`Download response ${e}`);
+    //   })
+    //   .catch((err) => {
+    //     logger(`Download err ${err}`);
+    //   });
+
+    console.error("Download complete!");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+})();
